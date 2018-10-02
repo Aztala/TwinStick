5065f61fb80131e6a7fd2a7f278e6d19edd4923d
Aztala <43759776+Aztala@users.noreply.github.com>
2018-10-02 15:13:28 -0400

    Added Controled ball Fixed X

---
+++ Assets/FollowCam.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+
+    public Vector3 currentposition;
+    public Vector3 DistanceFromBall;
+    public GameObject ball;
+	// Use this for initialization
+	void Start () {
+        Vector3 ballpos;
+        ball = GameObject.Find( "RollerBall" );
+        ballpos = ball.transform.position;
+        currentposition = this.transform.position;
+        DistanceFromBall = currentposition - ballpos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = DistanceFromBall + ball.transform.position;
+	}
+}
