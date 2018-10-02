d609a4d4d71c31e78fb8aa3733c675be9b590f5b
Aztala <43759776+Aztala@users.noreply.github.com>
2018-10-02 13:42:58 -0400

    CPIM

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CPIM : MonoBehaviour {
+public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +11,6 @@ public class CPIM : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        print(Input.GetAxis("Horizontal"));
 	}
 }
