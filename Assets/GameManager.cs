d722ac0cd5f48347674a50ca8c490e85f26067a9
Aztala <43759776+Aztala@users.noreply.github.com>
2018-10-08 21:24:55 -0400

    Pause game, level unlock, text

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     public bool isRecording = true;
-
+    private bool Gamepaused = false;
+    public Text GamePausedText;
 
 	// Use this for initialization
 	void Start () {
-		
+      
 	}
 	
 	// Update is called once per frame
@@ -22,5 +24,28 @@ public class GameManager : MonoBehaviour {
         {
             isRecording = true;
         }
+
+        if (Input.GetKeyDown( KeyCode.P ))
+        {
+            PauseGame();
+        }
 	}
+    public void PauseGame()
+    {
+        if (Gamepaused == false)
+        {
+            Gamepaused = !Gamepaused;
+            Time.timeScale = 0;
+            print( "Game is Paused as : " + Gamepaused );
+            GamePausedText.GetComponent<Text>().enabled = true;
+        }
+        else
+        {
+            Gamepaused = !Gamepaused;
+            Time.timeScale = 1;
+            print( "Game is Paused as : " + Gamepaused );
+            GamePausedText.GetComponent<Text>().enabled = false;
+
+        }
+    }
 }
--- Assets/PlayerPrefsManager.cs
+++ Assets/PlayerPrefsManager.cs
@@ -5,15 +5,44 @@ public class PlayerPrefsManager : MonoBehaviour {
 
 	const string MASTER_VOLUME_KEY = "master_volume";
 	const string DIFF_KEY = "difficulty";
-    const string LEVEL_UNLOCK = "lvl_unlock";
+    const string LEVEL_KEY = "lvl_unlock_";
 	const float volume = 1.0f;
 	const float diff = 1.0f;
 
 
+    #region Level unlock
+    public static void UnlockLevel(int level)
+    {
+        if (level <= Application.levelCount - 1 && level > 0)
+        {
+            PlayerPrefs.SetInt( LEVEL_KEY + level.ToString(), 1 );
+        }
+        else
+        {
+            Debug.LogError("Trying to unlock level not in build order!");
 
+        }
+    }
+    public static bool isLevelUnlock(int level)
+    {
+        int levelValue = PlayerPrefs.GetInt( LEVEL_KEY + level.ToString() );
+        bool isLevelUnlock = ( levelValue == 1 );
+        if (level <= Application.levelCount - 1 && level > 0)
+        {
+            return isLevelUnlock;
+        }
+        else
+        {
+            Debug.LogError( "The level you are looking for is not in the build order return false as Impossible to be unlock!" );
+            return false;
+        }
 
-	#region Master Volume
-	public static void SetMasterVolume(float vol)
+    }
+
+    #endregion
+
+    #region Master Volume
+    public static void SetMasterVolume(float vol)
 	{
 		if (vol >= 0f && vol <= 1f) {
 			PlayerPrefs.SetFloat (MASTER_VOLUME_KEY, vol);
